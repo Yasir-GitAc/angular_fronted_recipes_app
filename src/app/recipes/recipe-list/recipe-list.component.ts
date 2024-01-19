17bebe4552e3a16f3782930b2805d33a37612dc5
Yasir <ymd464673@gmail.com>
2024-01-19 22:30:42 +0600

    recipe_model_added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [],
+  imports: [RecipeItemComponent, CommonModule],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ]
 
 }
